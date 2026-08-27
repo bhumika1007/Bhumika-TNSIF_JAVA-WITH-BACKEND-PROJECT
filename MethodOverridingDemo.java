0b11af55a2e6c8d8eba4f91e3bc8b907141abda0
bhumika1007 <bhumikasridhar1007@gmail.com>
2026-08-27 14:11:32 +0530

    Add files via upload

---
+++ MethodOverridingDemo.java
@@ -0,0 +1,37 @@
+package polymorphismday2;
+
+//same method with same paratemeters
+
+class AnimalClass{
+	void sound() {
+		System.out.println("Animals can make sound");
+	}
+}
+
+
+class Lion extends AnimalClass{
+	void sound() {
+		System.out.println("Lion roars......");
+	}
+}
+
+public class MethodOverridingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnimalClass ac = new AnimalClass();
+		ac.sound();
+		
+		
+		/*
+		 * Lion l = new Lion(); l.sound();
+		 * 
+		
+		 */
+		
+		AnimalClass acl = new Lion();
+		acl.sound();
+
+	}
+
+}
