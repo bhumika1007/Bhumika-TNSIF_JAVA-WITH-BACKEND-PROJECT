9a2e435ce7deb0cc7754ff557ef30f442456dec7
bhumika1007 <bhumikasridhar1007@gmail.com>
2026-08-27 14:07:22 +0530

    Add files via upload

---
+++ WithoutExceptionHandling.java
@@ -0,0 +1,13 @@
+package exceptionhandling1;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the program continues....");
+		int data=100/0;
+		System.out.println("the program continues....");
+
+	}
+
+}
