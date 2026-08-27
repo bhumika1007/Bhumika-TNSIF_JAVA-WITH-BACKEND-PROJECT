3ea13e8565b68989bc1b82b0f68a17fbfdb8a787
bhumika1007 <bhumikasridhar1007@gmail.com>
2026-08-27 14:06:54 +0530

    Add files via upload

---
+++ Executor.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encapsulationnew ED = new Encapsulationnew();
+		ED.setName("M S Dhoni");
+		ED.setAge(44);
+		ED.setJersyNo(7);
+		
+		System.out.println(ED);
+		
+		Executor e = new Executor();
+		System.out.println(e);
+	}
+
+}
